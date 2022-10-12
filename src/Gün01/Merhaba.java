577bb56af14c80083e2361566a7128e6cbbc3ae3
kerem <keremcetinbas03@gmail.com>
2022-10-13 00:58:12 +0300

    ikinci taskımı bitirdim

--- src/Gün01/Merhaba.java
+++ src/Gün01/Merhaba.java
@@ -2,6 +2,11 @@ package Gün01;
 
 public class Merhaba {
     public static void main(String[] args) {
+        System.out.println("Merhaba");
+
+        for (int i=0;i<5;i++){
+            System.out.println("Merhaba "+i);
+        }
 
     }
 }
